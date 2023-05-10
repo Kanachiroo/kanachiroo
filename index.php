c48304c1983004d17e38e991542c64182587f518
Kanachiroo <132912334+Kanachiroo@users.noreply.github.com>
2023-05-10 10:44:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Olá, turma do az900";
-?>
+<!DOCTYPE html>
+<html>
+ <head> 
+   <litle>Minhas resenhas</little>
+   </head>
+  <body>
+    <h1>Minhas resenhas</h1>
+    <p>Bem vindo(a) à minha página inicial! Aqui você encontrará informações sobre mim e meus interesses. </p>
+    <p>Sou um(a) aluno(a) iniciante em programação e cloud computing!
+      Gosto de assistir seriados, jogar LOL, ir a academia e viajar para a praia.</p>
+    <a href="#" class="button">Entre em contato</a>
+  </body>
+    </html>
